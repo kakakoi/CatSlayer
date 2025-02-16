db58b0bb38051bcfafc027f6dc5a19058c7fd972
kakakoi <56661650+kakakoi@users.noreply.github.com>
2025-02-16 18:50:19 +0900

    fix: 🐛 test.tsのパラメータ型を修正

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,1 @@
-function test(x) {
-    const unused = 'test6';
-    const test = 'format error';
-    return x;
-}
+// テスト関数\nfunction test(x: unknown) {\n    const unused = "test6";\n    const test = "format error";\n    return x;\n}
