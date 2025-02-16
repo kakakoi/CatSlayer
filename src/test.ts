8569cc1c8e350eef9c97b82520512431ebc266c9
kakakoi <56661650+kakakoi@users.noreply.github.com>
2025-02-16 17:51:25 +0900

    test: simple-git-hooksのテスト

--- src/test.ts
+++ src/test.ts
@@ -3,4 +3,9 @@ function testFunction(   x:any   ):void{
 console.log(x)
     const unused   =  'unused variable';  // リントエラー（未使用変数）
         const num:number = "123";  // 型エラー（string を number に代入）
+}
+
+// 追加のフォーマットエラー
+const test2=(y:string)=>{
+return y+"test"
 } 
